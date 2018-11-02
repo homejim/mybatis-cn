8881a602577acb27a83666de907f3322f0b77ec2
homejim <454690042@qq.com>
2018-11-02 21:44:07 +0800

    GenericTokenParser-通用解析器

--- src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
+++ src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
@@ -34,33 +34,40 @@ public class GenericTokenParser {
     if (text == null || text.isEmpty()) {
       return "";
     }
-    // search open token
+    // 从第0位开始， 查找开始标记的下标
     int start = text.indexOf(openToken, 0);
-    if (start == -1) {
+    if (start == -1) { // 找不到则返回原参数
       return text;
     }
     char[] src = text.toCharArray();
+    // offset用来记录builder变量读取到了哪
     int offset = 0;
+    // builder 是最终返回的字符串
     final StringBuilder builder = new StringBuilder();
+    // expression 是每一次找到的表达式， 要传入处理器中进行处理
     StringBuilder expression = null;
     while (start > -1) {
       if (start > 0 && src[start - 1] == '\\') {
-        // this open token is escaped. remove the backslash and continue.
+        // 开始标记是转义的， 则去除转义字符'\'
         builder.append(src, offset, start - offset - 1).append(openToken);
         offset = start + openToken.length();
       } else {
-        // found open token. let's search close token.
+        // 此分支是找到了结束标记， 要找到结束标记
         if (expression == null) {
           expression = new StringBuilder();
         } else {
           expression.setLength(0);
         }
+        // 将开始标记前的字符串都添加到 builder 中
         builder.append(src, offset, start - offset);
+        // 计算新的 offset
         offset = start + openToken.length();
+
+        // 从此处开始查找结束的标记
         int end = text.indexOf(closeToken, offset);
         while (end > -1) {
           if (end > offset && src[end - 1] == '\\') {
-            // this close token is escaped. remove the backslash and continue.
+            // 此结束标记是转义的
             expression.append(src, offset, end - offset - 1).append(closeToken);
             offset = end + closeToken.length();
             end = text.indexOf(closeToken, offset);
@@ -71,16 +78,19 @@ public class GenericTokenParser {
           }
         }
         if (end == -1) {
-          // close token was not found.
+          // 找不到结束标记了
           builder.append(src, start, src.length - start);
           offset = src.length;
         } else {
+          // 找到了结束的标记， 则放入处理器进行处理
           builder.append(handler.handleToken(expression.toString()));
           offset = end + closeToken.length();
         }
       }
+      // 因为字符串中可能有很多表达式需要解析， 因此开始下一个表达式的查找
       start = text.indexOf(openToken, offset);
     }
+    // 最后一次未找到开始标记， 则将 offset 后的字符串添加到 builder 中
     if (offset < src.length) {
       builder.append(src, offset, src.length - offset);
     }
