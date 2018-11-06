21346ec26a5038803fa1de38d41bff85f61ae44a
homejim <454690042@qq.com>
2018-11-06 18:07:46 +0800

    ReflectorFactory 相关注释更新

--- src/main/java/org/apache/ibatis/reflection/DefaultReflectorFactory.java
+++ src/main/java/org/apache/ibatis/reflection/DefaultReflectorFactory.java
@@ -18,8 +18,13 @@ package org.apache.ibatis.reflection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+/**
+ * mybatis 提供的默认 ReflectorFactory 实现类
+ */
 public class DefaultReflectorFactory implements ReflectorFactory {
+  // 是否缓存
   private boolean classCacheEnabled = true;
+  // 使用 ConcurrentMap 缓存 Reflector 对象
   private final ConcurrentMap<Class<?>, Reflector> reflectorMap = new ConcurrentHashMap<>();
 
   public DefaultReflectorFactory() {
@@ -35,10 +40,16 @@ public class DefaultReflectorFactory implements ReflectorFactory {
     this.classCacheEnabled = classCacheEnabled;
   }
 
+  /**
+   *  如果开启缓存， 则从缓存中取出 Reflector 对象
+   *  否则创建一个新的
+   * @param type 需要创建 Reflector 对象对应的 Class
+   * @return 相应的 Reflector 对象
+   */
   @Override
   public Reflector findForClass(Class<?> type) {
     if (classCacheEnabled) {
-            // synchronized (type) removed see issue #461
+      // synchronized (type) removed see issue #461
       return reflectorMap.computeIfAbsent(type, Reflector::new);
     } else {
       return new Reflector(type);
--- src/main/java/org/apache/ibatis/reflection/ReflectorFactory.java
+++ src/main/java/org/apache/ibatis/reflection/ReflectorFactory.java
@@ -15,11 +15,25 @@
  */
 package org.apache.ibatis.reflection;
 
+/**
+ * 主要负责 Reflector 对象的创建和缓存
+ */
 public interface ReflectorFactory {
 
+  /**
+   * 检测该 ReflectorFactory 对象是否会缓存 Reflector 对象
+   */
   boolean isClassCacheEnabled();
 
+  /**
+   * 设置是否缓存
+   */
   void setClassCacheEnabled(boolean classCacheEnabled);
 
+  /**
+   * 创建指定 Class 对应的 Reflector 对象
+   * @param type
+   * @return
+   */
   Reflector findForClass(Class<?> type);
 }
