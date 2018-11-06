fcf618eee7d13d4cc914d5ec607f6df9cf15ba87
homejim <454690042@qq.com>
2018-11-06 23:34:43 +0800

    ObjectFactory用来创建对象

--- src/main/java/org/apache/ibatis/reflection/factory/ObjectFactory.java
+++ src/main/java/org/apache/ibatis/reflection/factory/ObjectFactory.java
@@ -20,19 +20,21 @@ import java.util.Properties;
 
 /**
  * MyBatis uses an ObjectFactory to create all needed new Objects.
- * 
+ * 使用该类来创建所需的新对象
  * @author Clinton Begin
  */
 public interface ObjectFactory {
 
   /**
    * Sets configuration properties.
+   * 设置配置信息
    * @param properties configuration properties
    */
   void setProperties(Properties properties);
 
   /**
-   * Creates a new object with default constructor. 
+   * Creates a new object with default constructor.
+   * 使用默认构造函数创建对象
    * @param type Object type
    * @return
    */
@@ -40,6 +42,7 @@ public interface ObjectFactory {
 
   /**
    * Creates a new object with the specified constructor and params.
+   * 通过指定的构造函数和参数， 创建一个对象
    * @param type Object type
    * @param constructorArgTypes Constructor argument types
    * @param constructorArgs Constructor argument values
@@ -48,6 +51,7 @@ public interface ObjectFactory {
   <T> T create(Class<T> type, List<Class<?>> constructorArgTypes, List<Object> constructorArgs);
   
   /**
+   *  检测制定类型是否为集合类型
    * Returns true if this object can have a set of other objects.
    * It's main purpose is to support non-java.util.Collection objects like Scala collections.
    * 
