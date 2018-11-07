26879bc93b962c4bd657d2547fe82d8f01156aac
homejim <454690042@qq.com>
2018-11-07 22:37:11 +0800

    DefaultObjectWrapperFactory： 默认 ObjectWrapperFactory 实现类， 不可用

--- src/main/java/org/apache/ibatis/reflection/wrapper/DefaultObjectWrapperFactory.java
+++ src/main/java/org/apache/ibatis/reflection/wrapper/DefaultObjectWrapperFactory.java
@@ -20,14 +20,17 @@ import org.apache.ibatis.reflection.ReflectionException;
 
 /**
  * @author Clinton Begin
+ * mybatis 提供的 ObjectWrapperFactory 的默认实现类（不可用）
  */
 public class DefaultObjectWrapperFactory implements ObjectWrapperFactory {
 
+  // 始终返回 false
   @Override
   public boolean hasWrapperFor(Object object) {
     return false;
   }
 
+  // 抛出异常
   @Override
   public ObjectWrapper getWrapperFor(MetaObject metaObject, Object object) {
     throw new ReflectionException("The DefaultObjectWrapperFactory should never be called to provide an ObjectWrapper.");
