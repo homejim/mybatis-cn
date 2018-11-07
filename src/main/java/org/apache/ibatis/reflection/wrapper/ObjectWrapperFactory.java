757ac6a20b0b2c177af50ddce6e2eef113d58227
homejim <454690042@qq.com>
2018-11-07 22:34:39 +0800

    ObjectWrapperFactory： 负责创建 ObjectWrapper 的对象

--- src/main/java/org/apache/ibatis/reflection/wrapper/ObjectWrapperFactory.java
+++ src/main/java/org/apache/ibatis/reflection/wrapper/ObjectWrapperFactory.java
@@ -19,11 +19,14 @@ import org.apache.ibatis.reflection.MetaObject;
 
 /**
  * @author Clinton Begin
+ * 负责 ObjectWrapper 对象的创建
  */
 public interface ObjectWrapperFactory {
 
+  // 判断是否有对应的 ObjectWrapper 对象
   boolean hasWrapperFor(Object object);
 
+  // 获取指定的 ObjectWrapper 对象
   ObjectWrapper getWrapperFor(MetaObject metaObject, Object object);
 
 }
