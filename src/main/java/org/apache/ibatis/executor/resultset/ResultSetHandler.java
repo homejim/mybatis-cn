f9506be372219cb676a60066910f87fd9cbab349
homejim <454690042@qq.com>
2018-11-10 17:58:46 +0800

    ResultSetHandler：结果集处理

--- src/main/java/org/apache/ibatis/executor/resultset/ResultSetHandler.java
+++ src/main/java/org/apache/ibatis/executor/resultset/ResultSetHandler.java
@@ -24,13 +24,17 @@ import java.util.List;
 
 /**
  * @author Clinton Begin
+ * 结果集处理
  */
 public interface ResultSetHandler {
 
+  // 处理生成的结果集
   <E> List<E> handleResultSets(Statement stmt) throws SQLException;
 
+  // 梳理结果集， 返回相应的游标对象
   <E> Cursor<E> handleCursorResultSets(Statement stmt) throws SQLException;
 
+  // 处理存储过程的输出函数
   void handleOutputParameters(CallableStatement cs) throws SQLException;
 
 }
