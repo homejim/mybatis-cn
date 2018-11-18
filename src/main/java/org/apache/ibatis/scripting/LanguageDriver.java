e876612613f44a77a3052ecae94b45e9fb28edb0
homejim <454690042@qq.com>
2018-11-18 22:48:37 +0800

    LanguageDriver

--- src/main/java/org/apache/ibatis/scripting/LanguageDriver.java
+++ src/main/java/org/apache/ibatis/scripting/LanguageDriver.java
@@ -27,7 +27,9 @@ public interface LanguageDriver {
 
   /**
    * Creates a {@link ParameterHandler} that passes the actual parameters to the the JDBC statement.
-   * 
+   *
+   * 创建 ParameterHandler
+   *
    * @param mappedStatement The mapped statement that is being executed
    * @param parameterObject The input parameter object (can be null) 
    * @param boundSql The resulting SQL once the dynamic language has been executed.
@@ -40,7 +42,9 @@ public interface LanguageDriver {
   /**
    * Creates an {@link SqlSource} that will hold the statement read from a mapper xml file. 
    * It is called during startup, when the mapped statement is read from a class or an xml file.
-   * 
+   *
+   * 创建 SqlSource 对象：从XML中读取 statetment
+   *
    * @param configuration The MyBatis configuration
    * @param script XNode parsed from a XML file
    * @param parameterType input parameter type got from a mapper method or specified in the parameterType xml attribute. Can be null.
@@ -51,7 +55,9 @@ public interface LanguageDriver {
   /**
    * Creates an {@link SqlSource} that will hold the statement read from an annotation.
    * It is called during startup, when the mapped statement is read from a class or an xml file.
-   * 
+   *
+   * 创建 SqlSource 对象：从注释中读取 statetment
+   *
    * @param configuration The MyBatis configuration
    * @param script The content of the annotation
    * @param parameterType input parameter type got from a mapper method or specified in the parameterType xml attribute. Can be null.
