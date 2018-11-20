ee077937b19ad0f7a13c4c01a57b11b40479be95
homejim <454690042@qq.com>
2018-11-20 23:39:14 +0800

    RawSqlSource

--- src/main/java/org/apache/ibatis/scripting/defaults/RawSqlSource.java
+++ src/main/java/org/apache/ibatis/scripting/defaults/RawSqlSource.java
@@ -28,7 +28,9 @@ import org.apache.ibatis.session.Configuration;
 /**
  * Static SqlSource. It is faster than {@link DynamicSqlSource} because mappings are 
  * calculated during startup.
- * 
+ *
+ * 负责静态语句的解析, 所以其速度很快
+ *
  * @since 3.2.0
  * @author Eduardo Macarron
  */
@@ -36,22 +38,38 @@ public class RawSqlSource implements SqlSource {
 
   private final SqlSource sqlSource;
 
+  /**
+   * 构造函数， 调用 getSql 获取SQL语句
+   */
   public RawSqlSource(Configuration configuration, SqlNode rootSqlNode, Class<?> parameterType) {
     this(configuration, getSql(configuration, rootSqlNode), parameterType);
   }
 
+  /**
+   * 构造函数
+   */
   public RawSqlSource(Configuration configuration, String sql, Class<?> parameterType) {
     SqlSourceBuilder sqlSourceParser = new SqlSourceBuilder(configuration);
+    // 如果没有参数类型， 则将参数类型定义为 Object
     Class<?> clazz = parameterType == null ? Object.class : parameterType;
+    // 返回的是 StaticSqlSource
     sqlSource = sqlSourceParser.parse(sql, clazz, new HashMap<String, Object>());
   }
 
+  /**
+   * 获取 SQL
+   */
   private static String getSql(Configuration configuration, SqlNode rootSqlNode) {
+    // 创建 DynamicContext 对象
     DynamicContext context = new DynamicContext(configuration, null);
+    // 解析
     rootSqlNode.apply(context);
     return context.getSql();
   }
 
+  /**
+   * 调用对应 sqlSource 的 getBoundSql 方法
+   */
   @Override
   public BoundSql getBoundSql(Object parameterObject) {
     return sqlSource.getBoundSql(parameterObject);
