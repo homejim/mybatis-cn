8697e376a098a35e4e25e21619b75166060d1f42
homejim <454690042@qq.com>
2018-12-13 22:44:49 +0800

    Invoker

--- src/main/java/org/apache/ibatis/reflection/invoker/Invoker.java
+++ src/main/java/org/apache/ibatis/reflection/invoker/Invoker.java
@@ -18,10 +18,13 @@ package org.apache.ibatis.reflection.invoker;
 import java.lang.reflect.InvocationTargetException;
 
 /**
+ * Invoker: 与方法的 invoke 相关
  * @author Clinton Begin
  */
 public interface Invoker {
+  // 调用方法
   Object invoke(Object target, Object[] args) throws IllegalAccessException, InvocationTargetException;
 
+  // 获取类型
   Class<?> getType();
 }
